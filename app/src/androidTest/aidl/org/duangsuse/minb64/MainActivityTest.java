cc58b5e22059169de5d6c57730d01efb1a239953
pigfromchina <fedora-opensuse@outlook.com>
2017-02-07 02:14:52 +0800

    new file:   .idea/modules.xml 	new file:   .idea/runConfigurations.xml 	new file:   .idea/vcs.xml 	new file:   app/app.iml 	new file:   app/src/androidTest/aidl/org/duangsuse/minb64/MainActivityTest.java 	new file:   minbase64.iml 	modified:   .gitignore 	modified:   app/app.iml 	modified:   app/build.gradle 	modified:   app/src/androidTest/aidl/org/duangsuse/minb64/MainActivityTest.java 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/org/duangsuse/minb64/MainActivity.java 	modified:   app/src/main/res/values-en/strings.xml 	modified:   app/src/main/res/values/styles.xml 	modified:   build.gradle 	.gradle/ 	.idea/compiler.xml 	.idea/copyright/ 	.idea/gradle.xml 	.idea/misc.xml 	.idea/workspace.xml 	app/build/ 	gradle/ 	gradlew 	gradlew.bat 	local.properties

---
+++ app/src/androidTest/aidl/org/duangsuse/minb64/MainActivityTest.java
@@ -0,0 +1,13 @@
+package org.duangsuse.minb64;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by arc on 2/7/17.
+ */
+public class MainActivityTest extends TestCase {
+    public void testMain() throws Exception {
+
+    }
+
+}
