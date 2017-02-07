9b5c0aa08129dea6700f2650d369a00155c5e548
duangsuse <fedora-opensuse@outlook.com>
2017-02-07 13:41:46 +0800

    添加变色状态栏支持(代码是抄来的,详见注释) add support for translucent status bar

--- app/src/main/java/org/duangsuse/minb64/MainActivity.java
+++ app/src/main/java/org/duangsuse/minb64/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.*;
 import android.content.*;
 import android.util.Base64;
 import android.view.View.*;
+import android.graphics.*;
 
 public class MainActivity extends Activity 
 {
@@ -14,7 +15,11 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+		//this.getActionBar().hide(); //使用主题的方法实现了
+		int color = Color.parseColor("#e8e8e8");
+		setColor(this , color); //这段调用的方法什么的都是我抄的.....感谢源:http://m.open-open.com/m/lib/view/1455584716230.html
 		Main();
 	}
 	void Main(){
@@ -77,4 +82,34 @@ public class MainActivity extends Activity
 		}
 		//return "It Works! Your option: "+flag;
 	}
+	/** * 设置状态栏颜色 * * @param activity 需要设置的activity * @param color 状态栏颜色值 */
+    public static void setColor(Activity activity, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 设置状态栏透明
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 生成一个状态栏大小的矩形
+            View statusView = createStatusView(activity, color);
+            // 添加 statusView 到布局中
+            ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+            decorView.addView(statusView);
+            // 设置根布局的参数
+            ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+            rootView.setFitsSystemWindows(true);
+            rootView.setClipToPadding(true);
+        }}
+	/** * 生成一个和状态栏大小相同的矩形条 * * @param activity 需要设置的activity * @param color 状态栏颜色值 * @return 状态栏矩形条 */
+    private static View createStatusView(Activity activity, int color) {
+        // 获得状态栏高度
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+
+        // 绘制一个和状态栏一样高的矩形
+        View statusView = new View(activity);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+																		 statusBarHeight);
+        statusView.setLayoutParams(params);
+        statusView.setBackgroundColor(color);
+        return statusView;
+    }
+    
 }
